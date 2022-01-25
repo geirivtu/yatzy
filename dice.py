7f8f3eedcba041a47aa1cbaf73774aac9ab8be9b
eirin <eirinkorvald.ek@gmail.com>
2022-01-25 19:23:54 +0100

    Made the names more international.

--- dice.py
+++ dice.py
@@ -4,11 +4,11 @@ from random import randint
 #Defining one die and creating a function for rolling it. 
 
 class Die:
-    def __init__(self,nr):
-        self._nr=nr
+    def __init__(self,no):
+        self._no=no
 
     def roll(self):
-        self._nr = randint(1,6)
+        self._no=randint(1,6)
 
-    def returnNr(self):
-        return self._nr
+    def returnNo(self):
+        return self._no
