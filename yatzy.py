7fdc0bc44e9b3cbdd2d81b1fbbd2aba9941a493a
eirin <eirinkorvald.ek@gmail.com>
2022-03-09 20:39:20 +0100

    Will try to use set more next time.

--- yatzy.py
+++ yatzy.py
@@ -11,49 +11,60 @@ def manyOfOne(number):
                 rollinghand.remove(dice)
     return resulthand
 
+def xofakind(number): #Five of a kind would be yahtsee
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
+    for i in range(3):
+        rollinghand.rolls()
+        
+        print(rollinghand)
+        #print(set(rollinghand))
+        if len(set(rollinghand))==1:
+            print("Yahtsee!")
+
+    return resulthand    
+
 def onepair(gameDice):
-    results=[]
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
+
 def twopairs(gameDice):
-    results=[]
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 
 def threeofakind(gameDice):
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def fourofakind():
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def lowstraight():
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def highstraight():
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def fullhouse():
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def chance():
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def yahtsee():
-    results=[]
-
+    rollinghand=Hand([1,2,3,4,5])
+    resulthand=Hand([])
 def yatzy():
     ones=manyOfOne(1)
-    print(ones)
     twos=manyOfOne(2)
-    print(twos)
     threes=manyOfOne(3)
-    print(threes)
     fours=manyOfOne(4)
-    print(fours)
     fives=manyOfOne(5)
-    print(fives)
     sixes=manyOfOne(6)
-    print(sixes)
     points=sum(ones)+sum(twos)+sum(threes)+sum(fours)+sum(fives)+sum(sixes)
     if points>=50:
         points+=50
     print(points)
+    xofakind(2)
 
 if __name__ == "__main__": #This will not be run if yatzygames gets imported to another file. 
     yatzy()
